2711c1fc918a069351f42bc211b97510f986718b
loloxwg <loloxwg@outlook.com>
2021-01-30 17:19:49 +0800

    项目开发及其初始化

---
+++ CloudRestaurant/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"CloudRestaurant/controller"
+	"CloudRestaurant/tools"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	cfg, err := tools.ParseConfig("./config/app.json")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	app := gin.Default()
+	registerRouter(app)
+	app.Run(cfg.AppHost + ":" + cfg.AppPort)
+}
+
+//路由设置
+func registerRouter(router *gin.Engine)  {
+	new(controller.HelloController).Router(router)
+}
